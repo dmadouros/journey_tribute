5997273d2514db6c1f953176201178501684a98c
David Madouros <dmadouros@comcast.net>
2015-10-17 12:17:40 -0600

    Allow venues to be edited
    - ugly, but working

--- app/controllers/admin/venues_controller.rb
+++ app/controllers/admin/venues_controller.rb
@@ -2,4 +2,24 @@ class Admin::VenuesController < Admin::AdminController
   def index
     @venues = Venue.all.order(:name)
   end
+
+  def edit
+    @venue = Venue.find(params[:id])
+  end
+
+  def update
+    @venue = Venue.find(params[:id])
+
+    if @venue.update_attributes(venue_params)
+      redirect_to admin_venues_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def venue_params
+    params.require(:venue).permit(:name, :address_1, :city, :state, :zip_code)
+  end
 end
--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -2,7 +2,6 @@ class ContactController < ApplicationController
   def create
     ContactMailer.contact(params).deliver_later
 
-
     redirect_to contact_url, flash: {thanks: true}
   end
 end
