825fbd58efa74f3ed4edfa1a945a06b258de650a
David Madouros <david.madouros@gmail.com>
2016-02-20 14:44:12 -0700

    Hide date/time of show and additional bands if not present

--- app/decorators/show_time_decorator.rb
+++ app/decorators/show_time_decorator.rb
@@ -1,10 +1,14 @@
 class ShowTimeDecorator < Draper::Decorator
 
   def date
+    return unless object.starts_at.present?
+
     object.starts_at.strftime('%A, %B %e, %Y')
   end
 
   def time
+    return unless object.starts_at.present?
+
     "#{object.starts_at.strftime('%-l:%M%P')} - #{object.ends_at.strftime('%-l:%M%P')}"
   end
 end
